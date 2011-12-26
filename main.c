41b9311fc9c4bc90e7b23bf4a7b8e6e00c000566
Vasco Conde <vasconde@gmail.com>
2011-12-26 23:58:55 +0000

    add new file system in main

--- main.c
+++ main.c
@@ -4,44 +4,55 @@
  * Autor: vasco conde
  */
 
+/*C PADRAO*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h> /*assert(codition)*/
 
+/*CBLAS*/
 #include <cblas.h>
 
+/*MODULOS STEREOM*/
 #include "matrixlib.h"
-
 #include "abmatching.h"
-
 #include "photojpeglib.h"
-
 #include "oex.h"
-
 #include "epipolar.h"
 
+/*CONSTANTES*/
+#define MAX_FILE_NAME 75
 
 int op;
 
-char file_l_c_i_l[50] = "\0";
-char file_l_c_t_l[50] = "\0";
-int l_c_l = 0;
+/*
+ * NOMES DOS FICHEIROS DOS DADOS DE ENTRADA
+ */
 
-char file_l_c_i_r[50] = "dados/par/limpres/pfs_fr.txt";
-char file_l_c_t_r[50] = "dados/par/limpres/pfs_tr.txt";
-int l_c_r = 14;
+char file_param_in_l[MAX_FILE_NAME]= "dados/dados/oin/oin_l.txt"; /*file parameters internal left*/
+char file_param_in_r[MAX_FILE_NAME]= "dados/dados/oin/oin_r.txt"; /*file parameters internal right*/
 
-char file_l_c_i_l_c[50] = "dados/epipolar/left.txt";
-char file_l_c_i_r_c[50] = "dados/epipolar/right.txt";
-int l_c_i_c = 12;
+char file_l_c_i_l[MAX_FILE_NAME] = "dados/dados/oex/pfs_fl.txt"; /*file list coordinates image left*/
+char file_l_c_t_l[MAX_FILE_NAME] = "dados/dados/oex/pfs_tl.txt"; /*file list coordinates terrain left*/
+int l_c_l = 19; /*list coordinates left*/
+char file_param_ex_ini_l[MAX_FILE_NAME]= "dados/dados/oex/oex_l.txt"; /*file parameters external initial left*/
 
-char file_image_l[50] = "dados/par/L_E00001_rect.jpg";
-char file_image_r[50] = "dados/par/R_E00003_rect.jpg";
+char file_l_c_i_r[MAX_FILE_NAME] = "dados/dados/oex/pfs_fr.txt"; /*file list coordinates image right*/
+char file_l_c_t_r[MAX_FILE_NAME] = "dados/dados/oex/pfs_tr.txt"; /*file list coordinates terrain right*/
+int l_c_r = 14; /*list coordinates right*/
+char file_param_ex_ini_r[MAX_FILE_NAME]= "dados/dados/oex/oex_r.txt"; /*file parameters external initial right*/
+
+char file_l_c_i_l_c[MAX_FILE_NAME] = "dados/dados/matrix/left.txt"; /*file list coordinates image left common*/
+char file_l_c_i_r_c[MAX_FILE_NAME] = "dados/dados/matrix/right.txt"; /*file list coordinates image right common*/
+int l_c_i_c = 12; /*list coordinates image common*/
+
+char file_image_l[MAX_FILE_NAME] = "dados/dados/imagens/L_E00001_rect.jpg"; /*left image*/
+char file_image_r[MAX_FILE_NAME] = "dados/dados/imagens/R_E00003_rect.jpg"; /*right image*/
 
 int dados_dis[7];
 
 
-
+/*apresenta na consola o menu principal*/
 int display_main_menu ()
 {
   int op;
@@ -49,85 +60,138 @@ int display_main_menu ()
     {
       printf("\n+--- STEREOM APP ---+\n\n");
       printf(" 1. Ficheiros de dados\n");
-      printf(" 2. Salvar dados\n");
-      printf(" 3. Orientacao Externa\n");
-      printf(" 4. Matriz Fundamental\n");
-      printf(" 5. Estereorestituicao automatica\n");
-      printf(" 6. Sobre StereoM\n");
-      printf(" 7. Sair\n");
+      printf(" 2. Orientacao Externa\n");
+      printf(" 3. Matriz Fundamental\n");
+      printf(" 4. Estereorestituicao automatica\n");
+      printf(" 5. Sobre StereoM\n");
+      printf(" 6. Sair\n");
       printf("\nopcao: ");
       scanf("%d", &op);
-    } while (op < 1 || op > 7);
+    } while (op < 1 || op > 6);
 
   return op;
 }
 
 void apresentar_dados()
 {
-  printf("\n*** Orientacao Externa ***\n");
-  printf("  Lista de coordenadas:\n");
-  printf("    Esquerda (%d):\n", l_c_l);
-  printf("      Imagem: %s\n", file_l_c_i_l);
-  printf("      Terreno: %s\n", file_l_c_t_l);
-  printf("    Direita (%d):\n", l_c_r);
-  printf("      Imagem: %s\n", file_l_c_i_r);
-  printf("      Terreno: %s\n", file_l_c_t_r);
-  printf("\n*** Matriz Fundamental ***\n");
-  printf("  Lista de coordenadas imagem comuns (%d):\n", l_c_i_c);
-  printf("    Esquerda: %s\n", file_l_c_i_l_c);
-  printf("    Direita: %s\n", file_l_c_i_r_c);
-  printf("\n*** Estereorestituicao automatica ***\n");  
-  printf("  Imagem esquerda: %s\n", file_image_l);
-  printf("  Imagem direita: %s\n", file_image_r);
+  printf("\n(0) *** Orientacao Interna ***\n");
+  printf("        Parametros:\n");
+  printf("          Esquerda: %s\n", file_param_in_l);
+  printf("          Direita: %s\n", file_param_in_r);
+  printf("\n(1) *** Orientacao Externa ***\n");
+  printf("        Esquerda (%d):\n", l_c_l);
+  printf("          Cordenadas imagem: %s\n", file_l_c_i_l);
+  printf("          Coordenadas terreno: %s\n", file_l_c_t_l);
+  printf("          Parametros iniciais: %s\n", file_param_ex_ini_l);
+  printf("        Direita (%d):\n", l_c_r);
+  printf("          Coordenadas imagem: %s\n", file_l_c_i_r);
+  printf("          Coordenadas terreno: %s\n", file_l_c_t_r);
+  printf("          Parametros iniciais: %s\n", file_param_ex_ini_r);
+  printf("\n(2) *** Matriz Fundamental ***\n");
+  printf("      Lista de coordenadas imagem comuns (%d):\n", l_c_i_c);
+  printf("        Esquerda: %s\n", file_l_c_i_l_c);
+  printf("        Direita: %s\n", file_l_c_i_r_c);
+  printf("\n(3) *** Estereorestituicao automatica ***\n");  
+  printf("        Imagem esquerda: %s\n", file_image_l);
+  printf("        Imagem direita: %s\n", file_image_r);
+}
+
+void modifica_dados ()
+{
+  int seccao;     /*seccao*/
+  int op_e_d; /*opecao */
+  printf("Seccao (1,2 ou 3): ");
+  
+  scanf("%d", &seccao);
+  assert(seccao >= 1 && seccao <= 3);
+
+  switch (seccao)
+    {
+    case 1:
+      printf("\n(1) *** Orientacao Externa ***\n");
+      printf("            Lista de coordenadas\n");
+      printf("1. Esquerda\n2. Direita\nopcao: ");
+  
+      scanf("%d", &op_e_d);
+      assert(op_e_d == 1 || op_e_d == 2);
+
+      switch (op_e_d)
+	{
+	case 1:
+	 
+	  printf("    Imagem da imagem esquerda: ");
+	  scanf("%s", file_l_c_i_l);  
+	  printf("    Terreno da imagem esquerda: ");
+	  scanf("%s", file_l_c_t_l);
+	  printf("numero de pontos: ");
+	  scanf("%d", &l_c_l);
+	  break;
+
+	case 2:
+	  printf("    Imagem da imagem direita: ");
+	  scanf("%s", file_l_c_i_r);
+	  printf("    Terreno da imagem direita:");
+	  scanf("%s", file_l_c_t_r);
+	  printf("    numero de pontos: ");
+	  scanf("%d", &l_c_r);
+
+	  break;
+	}
+      
+      break;
+    case 2:
+
+      printf("\n(2) *** Matriz Fundamental ***\n");
+      
+      printf("  Lista de coordenadas imagem comuns:\n");
+      printf("    Esquerda: ");
+      scanf("%s", file_l_c_i_l_c);
+      printf("    Direita: ");
+      scanf("%s", file_l_c_i_r_c);
+      printf("    numero de pontos: ");
+      scanf("%d", &l_c_i_c);
+
+      break;
+    case 3:
+
+      printf("\n(3) *** Estereorestituicao automatica ***\n");
+
+      printf("  Imagem esquerda: ");
+      scanf("%s", file_image_l);
+      printf("  Imagem direita: ");
+      scanf("%s", file_image_r);
+
+      break;
+    }
 }
 
 void ficheiros_dados ()
 {
   int o;
   do {
+
     apresentar_dados();
+
     printf("\n1. Alterar\n");
     printf("2. Concluido\n");
     printf("Opcao: ");
+    
     scanf("%d", &o);
+    assert(o == 1 || o == 2);
+
     switch (o)
       {
       case 1:
-	printf("\n*** Orientacao Externa ***\n");
-	printf("  Lista de coordenadas\n");
-	printf("    Imagem da imagem esquerda: ");
-	printf("ficheiro: ");
-	scanf("%s", file_l_c_i_l);  
-	printf("    Terreno da imagem esquerda: ");
-	scanf("%s", file_l_c_t_l);
-	printf("numero de pontos: ");
-	scanf("%d", &l_c_l);
-	printf("    Imagem da imagem direita: ");
-	scanf("%s", file_l_c_i_r);
-	printf("    Terreno da imagem direita:");
-	scanf("%s", file_l_c_t_r);
-	printf("    numero de pontos: ");
-	scanf("%d", &l_c_r);
-	printf("\n*** Matriz Fundamental ***\n");
-	printf("  Lista de coordenadas imagem comuns: ");
-	printf("    Esquerda:");
-	scanf("%s", file_l_c_i_l_c);
-	printf("    Direita:");
-	scanf("%s", file_l_c_i_r_c);
-	printf("    numero de pontos: ");
-	scanf("%d", &l_c_i_c);
-	printf("\n*** Estereorestituicao automatica ***\n");  
-	printf("  Imagem esquerda: ");
-	scanf("%s", file_image_l);
-	printf("  Imagem direita: ");
-	scanf("%s", file_image_r);
-	break;
-      default:
+	modifica_dados ();
 	break;
       }
   } while (o != 2);
 }
 
+/*
+ * Apresenta na consola a informacao relativa
+ * ao stereom
+ */
 void sobre_stereom ()
 {
   char c[1];
@@ -139,12 +203,8 @@ void sobre_stereom ()
   scanf("%s", c);
 }
 
- void libertar_memoria_toda ()
- {
-   
- }
 
-void orientacao_externa (char *ffoto, char *fterreno, int n, p_oex_oin_param oin_param, p_oex_param param)
+void orientacao_externa_foto (char *ffoto, char *fterreno, int n, p_oex_oin_param oin_param, p_oex_param param)
 {
   /*
   char * ffoto = "dados/par/limpres/pfs_fr.txt";
@@ -176,7 +236,7 @@ void orientacao_externa (char *ffoto, char *fterreno, int n, p_oex_oin_param oin
 
 void matriz_fundamental(double *FM)
 {
-  int np = l_c_i_c;
+  int np = l_c_i_c; /*list coordinates image common*/
 
   double *left = ml_alocar_M (np, 2);
   double *right = ml_alocar_M (np, 2);
@@ -276,12 +336,33 @@ int estereorestituicao_auto (double *FM, int tdim)
   return 0;
 }
 
+void orientacao_externa (p_oex_oin_param oin_param, p_oex_param oex_param)
+{
+  /*falta pedir os parametros iniciais e separar por direita e esquerda*/
+
+  if (oin_param == NULL)
+    oin_param = oex_alocar_oin_param ();
+  if (oex_param == NULL)
+    oex_param = oex_alocar_param ();
+  
+  oex_add_oin_param (oin_param, (2524.980712085470259+2512.961041523109998)/2, 1043.777785082072114, 782.329224502696547);
+  
+  oex_add_param (oex_param, 3, 1, 9, 0.0, 0.0, 0.0, 1);
+  
+  orientacao_externa_foto (file_l_c_i_r, file_l_c_t_r, 14, oin_param, oex_param);
+}
+
 int main (void)
 {
   double *FM = NULL;
 
-  p_oex_oin_param oin_param = NULL;
-  p_oex_param oex_param = NULL;
+  /*orientacoes para a foto da esquerda*/
+  p_oex_oin_param oin_param_l = NULL;
+  p_oex_param oex_param_l = NULL;
+  
+  /*orientacoes para a foto da direita*/
+  p_oex_oin_param oin_param_r = NULL;
+  p_oex_param oex_param_r = NULL;
 
   dados_dis[0] = 0; /*ficheiro coordenadas img esquerda*/
   dados_dis[1] = 0; /*ficheiro coordenadas img direita*/
@@ -299,45 +380,43 @@ int main (void)
       
       switch(op)
 	{
-	case 1:
+
+	case 1: /* 1. Ficheiros de dados */
 	  ficheiros_dados ();
 	  break;
-	case 2:
 
-	  break;
-	case 3:
-	  /*falta pedir os parametros iniciais e separar por direita e esquerda*/
-	  if (oin_param == NULL)
-	    oin_param = oex_alocar_oin_param ();
-	  if (oex_param == NULL)
-	    oex_param = oex_alocar_param ();
-
-	  oex_add_oin_param (oin_param, (2524.980712085470259+2512.961041523109998)/2, 1043.777785082072114, 782.329224502696547);
+	case 2: /* 2. Orientacao Externa */
 	  
-	  oex_add_param (oex_param, 3, 1, 9, 0.0, 0.0, 0.0, 1);
-
-	  orientacao_externa (file_l_c_i_r, file_l_c_t_r, 14, oin_param, oex_param);
+	  orientacao_externa (oin_param_l, oex_param_l);
+	  
 	  break;
-	case 4:
+
+	case 3: /* 3. Matriz Fundamental */
 	  if (FM == NULL) FM = ml_alocar_M (3, 3);
 	  matriz_fundamental(FM);
 	  oex_showm (FM, 3, 3);
 	  break;
-	case 5:
+
+	case 4: /* 4. Estereorestituicao automatica */
 
 	  estereorestituicao_auto (FM, 10);
 	  break;
-	case 6:
+
+	case 5: /* 5. Sobre StereoM */
 	  
 	  sobre_stereom ();
 	  break;
+
 	}
 
-    } while (op != 7);
+    } while (op != 6);
 
+  /*Libertacao de memoria*/
 
-  if (oin_param != NULL) oex_libertar_oin_param (oin_param);
-  if (oex_param != NULL) oex_libertar_param (oex_param);
+  if (oin_param_l != NULL) oex_libertar_oin_param (oin_param_l);
+  if (oex_param_l != NULL) oex_libertar_param (oex_param_l);
+  if (oin_param_r != NULL) oex_libertar_oin_param (oin_param_r);
+  if (oex_param_r != NULL) oex_libertar_param (oex_param_r);
   if (FM != NULL) free(FM);
   
 
